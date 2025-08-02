cd4eb90094c4e6fbfef609f7cfa44e4f3683bfaf
Ureonspw <Ureon206@gmail.com>
2025-08-02 20:21:25 +0000

    jjnj

--- barbershop/vite.config.js
+++ barbershop/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             input: 'resources/js/app.tsx',
             refresh: true,
             https: true,
+            Base: '/',
         }),
         react(),
     ],
